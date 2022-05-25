6e342469c8c5e7e291a4dedc4f212e9ca51c4b29
antony <aj21899@essex.ac.uk>
2022-05-25 16:55:38 +0100

    script

--- k8s/install_main.sh
+++ k8s/install_main.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Run the following scripts as sudo
 echo "Script to Install Kubernetes"
 
-apt-get update
-apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
+
+# To install the docker 
+# Caution: This will install the latest version of Docker
+# So it might install different latest versions of docker
+# if installed at different times.
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo rm get-docker.sh
+
+
 
